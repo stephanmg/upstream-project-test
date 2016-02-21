58f3c0c7a9d2a18f7718848cc6be381b263e8e8d
stephanmg <stephan@syntaktischer-zucker.de>
2016-02-21 20:17:54 +0100

    ...

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 travis whoami --skip-completion-check
 TOKEN=$(travis token --skip-completion-check)
 IFS=' ' read -r -a array <<< "$TOKEN"
-echo "$array{${#array[@]}}"
+echo "Token: ${array[${#array[@]}-1]}"
