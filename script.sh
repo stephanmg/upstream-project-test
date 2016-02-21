dd2e262efaaaf8592822f03119f730fce6c5e7b2
stephanmg <stephan@syntaktischer-zucker.de>
2016-02-21 19:50:01 +0100

    ...

--- script.sh
+++ script.sh
@@ -1,4 +1,3 @@
-travis login --help --skip-completion-check
-travis login --github-token b761ec3b94cab09116e992c35bee7a527092c24f 
+travis login --skip-complection-check --github-token b761ec3b94cab09116e992c35bee7a527092c24f 
 travis whoami --skip-completion-check
 travis token --skip-completion-check
