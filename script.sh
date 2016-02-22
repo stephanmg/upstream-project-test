b0b25e8733f9ace7297dd4c11be15f9573f80d33
stephanmg <stephan@syntaktischer-zucker.de>
2016-02-22 21:00:19 +0100

    Fix.

--- script.sh
+++
@@ -1,5 +0,0 @@
-travis whoami --skip-completion-check
-TOKEN=$(travis token --skip-completion-check)
-IFS=' ' read -r -a array <<< "$TOKEN"
-echo "Token: ${array[${#array[@]}-1]}"
-export MY_TOKEN=${array[${#array[@]}-1]}
