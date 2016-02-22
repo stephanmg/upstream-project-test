dbc224c6a7852c2d418fa76e41198d8edb9ae6f3
stephanmg <stephan@syntaktischer-zucker.de>
2016-02-22 20:54:02 +0100

    A test.

--- trigger-travis.sh
+++ trigger-travis.sh
@@ -69,8 +69,9 @@
 
 USER=$1
 REPO=$2
+ACCESS_TOKEN=$3
 
-travis login --skip-completion-check --github-token $TRAVIS_ACCESS_TOKEN
+travis login --skip-completion-check --github-token $ACCESS_TOKEN
 travis whoami --skip-completion-check
 TOKEN=$(travis token --skip-completion-check)
 IFS=' ' read -r -a array <<< "$TOKEN"
